006e67e8cced2bfe536340c1b7f9bdff3b8beab8
Broderick <broderio@umich.edu>
2023-11-09 19:18:07 +0000

    init

---
+++ ros2bag_to_array.py
@@ -0,0 +1,33 @@
+import rosbag2_py
+from rclpy.serialization import deserialize_message
+from rosidl_runtime_py.utilities import get_message
+from std_msgs.msg import String
+
+def bag_to_array(bagfile):
+    reader = rosbag2_py.SequentialReader()
+
+    storage_options = rosbag2_py.StorageOptions(uri=bagfile, storage_id="sqlite3")
+    converter_options = rosbag2_py.ConverterOptions(input_serialization_format="cdr",
+                                                    output_serialization_format="cdr")
+
+    reader.open(storage_options, converter_options)
+
+    topics_and_types = reader.get_all_topics_and_types()
+
+    # Create a dictionary mapping topic names to types
+    topic_types = {topic_metadata.name: topic_metadata.type for topic_metadata in topics_and_types}
+
+    data = []
+    while reader.has_next():
+        (topic, data, t) = reader.read_next()
+        msg_type = topic_types[topic]
+        msg = deserialize_message(data, get_message(msg_type))
+        print(msg)
+
+    return data
+
+def main():
+    bag_to_array('/root/path03-2/')
+
+if __name__ == '__main__':
+    main()
